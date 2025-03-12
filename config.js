d9abd3086effd04adf5d25f3418767c3d1efb3de
saqii453 <mola64669@gmail.com>
2025-03-12 16:45:05 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || OFxWmSSJ#ZX0AG-wtVwNJmaW_HgDYvFMcMa8MeLSl5El4lAqutaE
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
